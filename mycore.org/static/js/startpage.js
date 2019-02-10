e00ccc580b049552018629b9dec165bac704296a
Robert Stephan <rsteph-de@users.noreply.github.com>
2019-02-10 23:15:42 +0100

    layout and start page

---
+++ mycore.org/static/js/startpage.js
@@ -0,0 +1,23 @@
+ $(document).ready(function() {
+         $(".sphereLink").hover(
+
+              function () {
+                  var sphere = $(this).children('img.sphere');
+                  sphere.attr("src", "/images/start/abbildung_orange_110x110.jpg");
+              },
+              function () {
+                  var sphere = $(this).children('img.sphere');
+                  switch(sphere.attr("rel")){
+                      case "sphere_manysided":
+                          sphere.attr("src", "/images/start/abbildung_vielseitig.jpg");
+                          break;
+                      case "sphere_adaptable":
+                          sphere.attr("src", "/images/start/abbildung_anpassbar.jpg");
+                          break;
+                      case "sphere_lasting":
+                          sphere.attr("src", "/images/start/abbildung_nachhaltig.jpg");
+                          break;
+                  }
+              }
+          );
+      });
