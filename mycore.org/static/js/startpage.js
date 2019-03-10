51f04cce18ea7cb62f1095480c2fdc5171677c43
Robert Stephan <rsteph-de@users.noreply.github.com>
2019-03-10 15:55:58 +0100

    NEWS as JSON Data

--- mycore.org/static/js/startpage.js
+++ mycore.org/static/js/startpage.js
@@ -3,19 +3,19 @@
 
               function () {
                   var sphere = $(this).children('img.sphere');
-                  sphere.attr("src", "/images/start/abbildung_orange_110x110.jpg");
+                  sphere.attr("src", "./images/start/abbildung_orange_110x110.jpg");
               },
               function () {
                   var sphere = $(this).children('img.sphere');
                   switch(sphere.attr("rel")){
                       case "sphere_manysided":
-                          sphere.attr("src", "/images/start/abbildung_vielseitig.jpg");
+                          sphere.attr("src", "./images/start/abbildung_vielseitig.jpg");
                           break;
                       case "sphere_adaptable":
-                          sphere.attr("src", "/images/start/abbildung_anpassbar.jpg");
+                          sphere.attr("src", "./images/start/abbildung_anpassbar.jpg");
                           break;
                       case "sphere_lasting":
-                          sphere.attr("src", "/images/start/abbildung_nachhaltig.jpg");
+                          sphere.attr("src", "./images/start/abbildung_nachhaltig.jpg");
                           break;
                   }
               }
